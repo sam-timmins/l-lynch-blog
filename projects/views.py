d4f947ef9c6fff91de255060fb20e3ac62b1462b
Sam Timmins <sam.timmins1@gmail.com>
2022-07-17 20:52:54 +0100

    Add all projects to context

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Projects
 
 
 def projects(request):
     """ A view to return the projects page """
 
-    return render(request, 'projects/projects.html')
+    all_projects = Projects.objects.all()
+
+    context = {
+        'all_projects': all_projects,
+    }
+
+    return render(request, 'projects/projects.html', context)
