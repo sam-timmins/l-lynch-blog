20fd057b6aebb692b9f19c9aad7adb371bed92c5
Sam Timmins <sam.timmins1@gmail.com>
2022-07-17 20:52:39 +0100

    Add description to projects and migrate

--- projects/models.py
+++ projects/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from ckeditor.fields import RichTextField
 
 class Projects(models.Model):
     """ Projects model """
 
     name = models.CharField(
         max_length=254,
+        blank=False,
+        null=False,
+        unique=True,
     )
+
+    description = RichTextField(
+        null=True,
+        blank=True,
+    )
+
     slug = models.SlugField(
         null=True,
         blank=True,
@@ -15,7 +25,7 @@ class Projects(models.Model):
 
     def __str__(self):
         return str(self.name)
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
