d194c5639b1873a296798442a2f3432890f2ae4a
Sam Timmins <sam.timmins1@gmail.com>
2022-07-17 20:17:01 +0100

    Add projects model and migrate

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+class Projects(models.Model):
+    """ Projects model """
+
+    name = models.CharField(
+        max_length=254,
+    )
+    slug = models.SlugField(
+        null=True,
+        blank=True,
+        unique=True
+    )
+
+    def __str__(self):
+        return str(self.name)
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
