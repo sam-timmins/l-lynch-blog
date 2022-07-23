87ff6011c583a1db94ba3ad7b8dd947fd1a48e5b
Sam Timmins <sam.timmins1@gmail.com>
2022-07-23 21:03:47 +0100

    remove uploader

--- blog/settings.py
+++ blog/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'ckeditor_uploader',
+    'ckeditor',
 
     'home',
     'projects',
