05545002f899ef3d3fc3eaf30442d4305cd37d3e
Sam Timmins <sam.timmins1@gmail.com>
2022-07-24 10:59:14 +0100

    Add associated blogs count

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from blogs.models import Blogs
 from .models import Projects
 
 
@@ -7,12 +8,28 @@ def projects(request):
     """ A view to return the projects page """
 
     all_projects = Projects.objects.all()
+    blogs = Blogs.objects.all()
+
+    projects_list = []
+    project_totals = {}
+
+    for blog in blogs:
+        for blog_projects in blog.project.all():
+
+            projects_list.append(blog_projects)
+
+            total = projects_list.count(blog_projects)
+
+            project_totals.update({blog_projects : total})
+
 
     projects_count = all_projects.count()
 
     context = {
         'all_projects': all_projects,
         'projects_count': projects_count,
+        'project_totals': project_totals,
+        'blogs': blogs,
     }
 
     return render(request, 'projects/projects.html', context)
