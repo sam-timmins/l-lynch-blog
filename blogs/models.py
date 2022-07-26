e11172df7b02f88125c4f3ae04cbaa538dba3306
Sam Timmins <sam.timmins1@gmail.com>
2022-07-26 21:18:40 +0100

    Add image caption to model and migrate

--- blogs/models.py
+++ blogs/models.py
@@ -16,6 +16,12 @@ class Images(models.Model):
         null=True,
     )
 
+    caption = models.CharField(
+        max_length=254,
+        blank=False,
+        null=True,
+    )
+
     def __str__(self):
         return str(self.description)
 
