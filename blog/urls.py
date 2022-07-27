32ea8519e19be8311acec7a95ff91a998d060480
Sam Timmins <sam.timmins1@gmail.com>
2022-07-27 20:35:30 +0100

    aDD CONTACT URLS

--- blog/urls.py
+++ blog/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('projects/', include('projects.urls')),
     path('blogs/', include('blogs.urls')),
+    path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
