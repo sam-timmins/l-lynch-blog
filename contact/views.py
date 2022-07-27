a9b63ec889400062e59f50b86a08d760c6d1719a
Sam Timmins <sam.timmins1@gmail.com>
2022-07-27 20:39:00 +0100

    Set up basic contact view

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def contact(request):
+    """ contact page """
+
+
+    context = {
+
+    }
+
+    return render(request, 'contact/contact.html', context)
