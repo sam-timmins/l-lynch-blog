fc84da2ae1fecdeffa32962a4662acfb33c5fa31
Sam Timmins <sam.timmins1@gmail.com>
2022-08-15 15:01:46 +0100

    Change projects to topics on frontend

--- blog/urls.py
+++ blog/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('projects/', include('projects.urls')),
+    path('topics/', include('projects.urls')),
     path('blogs/', include('blogs.urls')),
     path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
